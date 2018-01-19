f672b7302e7ce47f7c9c74e2f2d19677292a4cfa
sohailmerchant <13290932+sohailmerchant@users.noreply.github.com>
2018-01-19 10:47:54 +0000

    markdown correction

--- web-worker/data-worker.js
+++ web-worker/data-worker.js
@@ -129,6 +129,8 @@ function parseText(pageStr, bookName) {
   return data;
 }
 function filterBookNoise(text) {
+  text = text.replace(/\(@\)/g, "\n"); 
+  text.replace(/\(@@\)/g, " #");
   text = text.replace(/\n~~/g, ' ');
   text = text.replace(/ +/g, ' ');
   text = text.replace(/### \|+/g, function (match) {
